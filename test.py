584ffb239e3da056429d213bd95690a453ef67e4
Pardis-hub <razaghian.pardis@gmail.com>
2026-02-02 23:24:13 +0330

    Delete test

--- test.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-
-service = Service(
-    "E:/Work/Python/python_masterclass_projects/chromedriver-win64/chromedriver.exe"
-)
-
-driver = webdriver.Chrome(service=service)
-driver.get("https://www.python.org")
